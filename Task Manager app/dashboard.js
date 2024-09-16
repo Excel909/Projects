ede0156a0edc9c58f0d452c34c6ea0a947bd2909
Excel909 <excellentayomide@gmail.com>
2024-09-16 13:37:19 +0100

    make heavy progress

---
+++ Task Manager app/dashboard.js
@@ -0,0 +1,15 @@
+// To handle all dashboard functionalities
+
+// to make dashboard creator appear and dissapear 
+
+let task_show_btn = document.getElementById('add_task');
+let dash_creator = document.getElementById('dash-creator');
+let dash_exit_btn = document.getElementById('ta-exit-btn');
+
+task_show_btn.addEventListener('click', () => {
+    dash_creator.style.display = 'block';
+});
+
+dash_exit_btn.addEventListener('click', () => {
+    dash_creator.style.display = 'none';
+});
